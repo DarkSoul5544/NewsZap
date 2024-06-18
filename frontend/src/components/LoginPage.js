16e2d92c71a3176f3072f48ff00375c602ae40e8
Vishal <vishal.it63@gmail.com>
2024-06-18 21:32:47 +0530

    testing

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -13,7 +13,7 @@ const LoginPage = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.post("http://localhost:5000/api/login", { email, password });
+      const response = await axios.post("https://news-zap.vercel.app/api/login", { email, password });
 
       if (response.data.token) {
         localStorage.setItem('token', response.data.token);
