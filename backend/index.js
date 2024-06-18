21d80e76931ffd8ba3e755945617b9d08c443eca
Vishal <vishal.it63@gmail.com>
2024-06-18 21:43:02 +0530

    patch

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
 const Razorpay = require('razorpay');
 const dotenv = require('dotenv');
 dotenv.config();
@@ -12,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-
+app.use(cors());
 
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URL, {
